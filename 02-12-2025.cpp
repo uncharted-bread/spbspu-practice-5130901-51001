1520a8348a1e45477455c894f453bc148c178eaa
uncharted-bread <gordeychikmika@gmail.com>
2025-12-02 23:06:13 +0300

    Complete code from the lesson

--- 02-12-2025.cpp
+++ 02-12-2025.cpp
@@ -1,106 +1,174 @@
 #include <iostream>
 #include <stdexcept>
 namespace topit {
-	struct p_t {
-		int x, y;
-	};
-	struct f_t {
-		p_t aa, bb;
-	};
-	bool operator == (p_t a, p_t b);
-	bool operator != (p_t a, p_t b);
-	struct IDraw {
-		virtual ~IDraw() = default;
-		virtual p_t begin() const = 0;
-		virtual p_t next(p_t prev) const = 0;
-	};
-	struct Dot : IDraw {
-		explicit Dot(p_t dd);
-		p_t begin() const override;
-		p_t next(p_t prex) const = 0;
-		p_t d;
-	};
-	void append(const IDraw* sh, p_t** ppts, size_t& s);
-	f_t frame(const p_t* pts, size_t s);
-	char* canvas(f_t fr);
-	void paint(p_t p, char* cnv, f_t fr, char fill);
-	void flush(std::ostream& os, const char* cnv, f_t fr);
 
+    struct p_t {
+        int x, y;
+    };
+
+    struct f_t {
+        p_t aa, bb;
+    };
+    size_t rows(f_t fr);
+    size_t cols(f_t fr);
+    bool operator==(p_t a, p_t b);
+    bool operator!=(p_t a, p_t b);
+
+    struct IDraw {
+        virtual ~IDraw() = default;
+        virtual p_t begin() const = 0;
+        virtual p_t next(p_t prev) const = 0;
+    };
+    p_t* extend(const p_t* pts, size_t s, p_t fill);
+    void extend(p_t** pts, size_t& s, p_t fill);
+    struct Dot : IDraw {
+        explicit Dot(p_t dd);
+        p_t begin() const override;
+        p_t next(p_t prev) const override;
+        p_t d;
+    };
+    void append(const IDraw* sh, p_t** ppts, size_t& s);
+    f_t frame(const p_t* pts, size_t s);
+    char* canvas(f_t fr, char fill);
+    void paint(p_t p, char* cnv, f_t fr, char fill);
+    void flush(std::ostream& os, const char* cnv, f_t fr);
 }
+
 int main() {
-	using namespace topit;
-	IDraw* shp[3] = {}; 
-	p_t* pts = nullptr;
-	size_t s = 0;
-	try {
-		shp[0] = new Dot({ 0,0 });
-		shp[1] = new Dot({ 2, 3 });
-		shp[2] = new Dot({ -5, -2 });
-		for (size_t i = 0; i < 3; ++i) {
-			append(shp[i], &pts, s);
-		}
-		f_t fr = frame(pts, s);
-		char* cnv = canvas(fr, '.');
-		for (size_t i = 0, i < s; ++i;) {
-			paint(pts[1], cnv, fr, '#');
-		}
-		flush(std::cout, cnv, fr);
-		delete[] cnv;
-	}
-	catch {} {
-		std::cerr << "Error!\n";
-		err = 1;
-	}
-	delete shp[2];
-	delete shp[1];
-	delete shp[0];
-	return err;
+    using namespace topit;
+    int err = 0;
+    IDraw* shp[3] = {};
+    p_t* pts = nullptr;
+    size_t s = 0;
+    try {
+        shp[0] = new Dot({ 0, 0 });
+        shp[1] = new Dot({ 2, 3 });
+        shp[2] = new Dot({ -6, -2 });
+        for (size_t i = 0; i < 3; ++i) {
+            append(shp[i], &pts, s);
+        }
+        f_t fr = frame(pts, s);
+        char* cnv = canvas(fr, '.');
+        for (size_t i = 0; i < s; ++i) {
+            paint(pts[i], cnv, fr, '#');
+        }
+        flush(std::cout, cnv, fr);
+        delete[] cnv;
+    }
+    catch (...) {
+        std::cerr << "Error!\n";
+        err = 1;
+    }
+    delete shp[2];
+    delete shp[1];
+    delete shp[0];
+    return err;
 }
-void topit::flush(std::ostream& os, const char* cnv, f_t fr) {
 
+void topit::paint(p_t p, char* cnv, f_t fr, char fill)
+{
+    size_t dx = p.x - fr.aa.x;
+    size_t dy = fr.bb.y - p.y;
+    cnv[dy * cols(fr) + dx] = fill;
 }
-char* topit::canvas(f_t fr, char fill) {
-	size_t s = rows(fr) * cols(fr);
-	char* c = new char[s];
-	for (size_t i = 0; i < s; ++i) {
-		c[i] = fill;
-	}
+
+topit::p_t* topit::extend(const p_t* pts, size_t s, p_t fill)
+{
+    p_t* r = new p_t[s + 1];
+    for (size_t i = 0; i < s; i++) {
+        r[i] = pts[i];
+    }
+    r[s] = fill;
+    return r;
 }
-topit::f_t topit::frame(const p_t* ts, size_t s) {
-	int minx = pts[0].x, miny = pts[0].y;
-	int maxx = minx, maxy = miny;
-	for (size_t i = 1; i < s; ++i) {
-		minx = std::min(minx, pts[i].x);
-		miny = std::min(miny, pts[i].y);
-		maxx = std::max(maxx, pts[i].x);
-		maxy = std::max(maxy, pts[i].y);
-	}
-	p_t a{minx, miny};
-	p_t b maxx, maxy};
-	return f_t{a, b};
+
+void topit::extend(p_t** pts, size_t& s, p_t fill)
+{
+    p_t* r = extend(*pts, s, fill);
+    delete[] * pts;
+    ++s;
+    *pts = r;
 }
-topit::Dot::Dot(p_t dd):
-	IDraw(),
-	d{dd}
-{ }
+
+void topit::append(const IDraw* sh, p_t** ppts, size_t& s)
+{
+    extend(ppts, s, sh->begin());
+    p_t b = sh->begin();
+
+    while (sh->next(b) != sh->begin()) {
+        b = sh->next(b);
+        extend(ppts, s, b);
+    }
+}
+
+void topit::flush(std::ostream& os, const char* cnv, f_t fr)
+{
+    for (size_t i = 0; i < rows(fr); ++i) {
+        for (size_t j = 0; j < cols(fr); ++j) {
+            os << cnv[i * cols(fr) + j];
+        }
+        os << '\n';
+    }
+}
+
+char* topit::canvas(f_t fr, char fill)
+{
+    size_t s = rows(fr) * cols(fr);
+    char* c = new char[s];
+    for (size_t i = 0; i < s; ++i) {
+        c[i] = fill;
+    }
+    return c;
+}
+
+topit::f_t topit::frame(const p_t* pts, size_t s)
+{
+    int minx = pts[0].x;
+    int miny = pts[0].y;
+    int maxx = pts[0].x;
+    int maxy = pts[0].y;
+    for (size_t i = 0; i < s; ++i) {
+        minx = std::min(minx, pts[i].x);
+        miny = std::min(miny, pts[i].y);
+        maxx = std::max(maxx, pts[i].x);
+        maxy = std::max(maxy, pts[i].y);
+    }
+    p_t a{ minx, miny };
+    p_t b{ maxx, maxy };
+    return f_t{ a, b };
+}
+
+topit::Dot::Dot(p_t dd) :
+    IDraw(),
+    d{ dd }
+{
+}
+
 topit::p_t topit::Dot::begin() const {
-	return d;
+    return d;
 }
-topit::p_t topit::Dot::next(p_t prev) const; {
-	if (prex != d) {
-		throw std::logic_error("bad prev")
-	}
-	return d;
+
+topit::p_t topit::Dot::next(p_t prev) const {
+    if (prev != d) {
+        throw std::logic_error("bad prev");
+    }
+    return d;
 }
-size_t topit::rows(f_t fr) {
-	return fr.bb.y - fr.aa.y + 1;
-}
-size_t topit::cols(f_t fr) {
-	return fr.bb.x - fr.aa.x + 1;
-}
+
 bool topit::operator==(p_t a, p_t b) {
-	return a.x == b.x && a.y == b.y;
+    return a.x == b.x && a.y == b.y;
 }
+
 bool topit::operator!=(p_t a, p_t b) {
-	return !(a == b);
+    return !(a == b);
 }
+
+size_t topit::rows(f_t fr)
+{
+    return fr.bb.y - fr.aa.y + 1;
+}
+size_t topit::cols(f_t fr)
+{
+    return fr.bb.x - fr.aa.x + 1;
+}
+
